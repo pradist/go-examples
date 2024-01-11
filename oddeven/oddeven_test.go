d4bf2aa54f19cd00a771ae80a510b9186c91aa4d
Pradist Keawborisut <pradist.ke@gmail.com>
2024-01-11 20:48:22 +0700

    [oddeven] refector: change test pattern

--- oddeven/oddeven_test.go
+++ oddeven/oddeven_test.go
@@ -2,15 +2,28 @@ package oddeven_test
 
 import (
 	"go-examples/oddeven"
-	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
-func TestWhenInputIs1ShouldRetuenOdd(t *testing.T) {
-	expected := "Odd"
+func TestOddEvenFind(t *testing.T) {
+	type args struct {
+		input int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"When input is 1 should retuen odd", args{1}, "Odd"},
+	}
 
-	actual := oddeven.Find(1)
-
-	assert.Equal(t, expected, actual)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := oddeven.Find(test.args.input)
+			if actual != test.want {
+				t.Errorf("Expected %s but it got %s", test.want, actual)
+			}
+		})
+	}
 }
