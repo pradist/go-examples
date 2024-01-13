0858ed720aa8e01eaae9f828106d35bc17261a17
Pradist Keawborisut <pradist.ke@gmail.com>
2024-01-13 14:53:50 +0700

    [captcha] refactor: left operand #2

--- captcha/captcha.go
+++ captcha/captcha.go
@@ -3,14 +3,5 @@ package captcha
 import "strconv"
 
 func LeftOperand(pattern, left int) string {
-	if left == 2 {
-		return strconv.Itoa(left)
-	}
-	if left == 3 {
-		return strconv.Itoa(left)
-	}
-	if left == 4 {
-		return strconv.Itoa(left)
-	}
 	return strconv.Itoa(left)
 }
