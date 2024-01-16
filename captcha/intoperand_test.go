e469df49e3c327a7accfa21a78bdf10e77669ff6
Pradist Keawborisut <pradist.ke@gmail.com>
2024-01-16 22:39:19 +0700

    [captcha] feat: add test case for Int Operand

--- captcha/intoperand_test.go
+++ captcha/intoperand_test.go
@@ -11,3 +11,13 @@ func TestIntOperand_WhenOperandIs1_ShouldReturn1(t *testing.T) {
 	operand := captcha.IntOperand{Operand: 1}
 	assert.Equal(t, "1", operand.ToString())
 }
+
+func TestIntOperand_WhenOperandIs5_ShouldReturn5(t *testing.T) {
+	operand := captcha.IntOperand{Operand: 2}
+	assert.Equal(t, "2", operand.ToString())
+}
+
+func TestIntOperand_WhenOperandIs9_ShouldReturn9(t *testing.T) {
+	operand := captcha.IntOperand{Operand: 9}
+	assert.Equal(t, "9", operand.ToString())
+}
